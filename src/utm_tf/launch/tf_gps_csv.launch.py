3cbe1cc27fec649ed02ddd695fa9d4a4e08c6240
Hannibal730 <cds730@naver.com>
2026-03-03 19:55:43 +0900

    🎨 refactor: tf_gps_csv.launch.py에서 노드 추가 및 total.launch.py 삭제

--- src/utm_tf/launch/tf_gps_csv.launch.py
+++ src/utm_tf/launch/tf_gps_csv.launch.py
@@ -3,14 +3,41 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-def generate_launch_description():
-    # Get the path to the package's share directory
-    utm_tf_share_dir = get_package_share_directory('utm_tf')
 
-    # Define the path to the parameters file
+def generate_launch_description():
+    utm_tf_share_dir = get_package_share_directory('utm_tf')
     params_file = os.path.join(utm_tf_share_dir, 'config', 'tf_gps_csv.yaml')
 
-    # Declare the node
+    f9p_node = Node(
+        package='utm_tf',
+        executable='f9p_to_utm',
+        name='f9p_to_utm',
+        output='screen',
+        remappings=[
+            ('/f9p_utm', '/gps/f9p_utm'),
+        ]
+    )
+
+    f9r_node = Node(
+        package='utm_tf',
+        executable='f9r_to_utm',
+        name='f9r_to_utm',
+        output='screen',
+        remappings=[
+            ('/f9r_utm', '/gps/f9r_utm'),
+        ]
+    )
+
+    azimuth_node = Node(
+        package='utm_tf',
+        executable='azimuth_angle_calculator_node',
+        name='azimuth_angle_calculator_node',
+        output='screen',
+        remappings=[
+            ('/azimuth_angle', '/gps/azimuth_angle'),
+        ]
+    )
+
     tf_gps_csv_node = Node(
         package='utm_tf',
         executable='tf_gps_csv_node',
@@ -27,5 +54,8 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
+        f9p_node,
+        f9r_node,
+        azimuth_node,
         tf_gps_csv_node
     ])
