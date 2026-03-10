ac18cabcbb92e2708dbb35698f783767130e795d
Hannibal730 <cds730@naver.com>
2026-03-10 14:33:59 +0900

    🚀 exp: 0309 노천극장 차선주행 실험결과 (차선 ld 최소 1.8이어야 할 것 같음)

--- src/yolotl_ros2/yolotl_ros2/utils/bev_scale_check.py
+++ src/yolotl_ros2/yolotl_ros2/utils/bev_scale_check.py
@@ -347,7 +347,7 @@ def main(args=None):
     package_share_directory = get_package_share_directory('yolotl_ros2')
 
     parser.add_argument('--topic', type=str, default='/image_raw/compressed')
-    parser.add_argument('--bev-filename', type=str, default='bev_params_0308.npz')
+    parser.add_argument('--bev-filename', type=str, default='bev_params_hsj.npz')
     parser.add_argument('--interval-m', type=float, default=0.3)
     parser.add_argument('--out-npz', type=str, default='bev_scale_result.npz')
     parser.add_argument('--out-txt', type=str, default='bev_scale_result.txt')
